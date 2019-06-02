5ec79d5022decbec3afc596baed5673691673f4d
aryyya <aryyya.xyz@gmail.com>
2019-06-01 19:55:47 -0700

    Prevent deletion of categories that contain items.

--- common/models/category.js
+++ common/models/category.js
@@ -2,4 +2,13 @@
 
 module.exports = function(Category) {
 
+  Category.observe('before delete', context => {
+    return Category.app.models.Product
+      .count({ categoryId: context.where.id })
+      .then(productsCount => {
+        if (productsCount > 0) {
+          return Promise.reject('error deleting category with products')
+        }
+      })
+  })
 };
